6099eb337cb7daad7dfb2576cc7d3351d8694625
lxhsjtu <843829411@qq.com>
2019-09-17 10:55:42 +0800

    Revert "[TOPI] Setting up AutoTVM template for Intel Int8 conv2D (#3955)"
    This reverts commit 3edf52607b1209fb0d10f2dcc53ebe7cd177042d.

--- topi/python/topi/nn/conv2d.py
+++ topi/python/topi/nn/conv2d.py
@@ -669,6 +669,7 @@ def conv2d_NCHWc_int8_compute(data, kernel, strides, padding, dilation, layout, 
                        name='conv2d_NCHWc_int8', tag="conv2d_NCHWc_int8")
 
 
+
 def conv2d_winograd_weight_transform(kernel, tile_size):
     """Weight transformation for winograd
 
